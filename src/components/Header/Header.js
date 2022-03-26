df6c71f7e4ffca457463262d28e78464a3ed343e
Palak-SweetCoder <pspalak6@gmail.com>
2022-03-26 13:27:20 +0600

    header completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        
+    <div className="App">
+      <header className="App-header text-center p-2 text-white">
+        <h1>Luxury Shopping Center</h1>
+        <h3>Choose four product to generate best one</h3>
+      </header>
+    </div>
+    
+    );
+};
+
+export default Header;
